529939afa474c7a71b339e933c58e77fca28e6b0
joaoh <joaoh@noreply.codeberg.org>
2024-10-02 15:01:59 +0000

    Upload files to "/"

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using ViaCEP.Services;
+using ViaCEP.Interfaces;
+using ViaCEP.Models;
+using Lolcat;
+
+namespace ViaCEP
+{
+    public class Program
+    {
+        static readonly string ASCIIART = @" __     ___        ____ _____ ____   ____ _     ___ 
+ \ \   / (_) __ _ / ___| ____|  _ \ / ___| |   |_ _|
+  \ \ / /| |/ _` | |   |  _| | |_) | |   | |    | | 
+   \ V / | | (_| | |___| |___|  __/| |___| |___ | | 
+    \_/  |_|\__,_|\____|_____|_|    \____|_____|__|";
+
+        static async Task Main()
+        {
+            var style = new RainbowStyle();
+            var rainbow = new Rainbow(style);
+
+            rainbow.WriteLineWithMarkup(ASCIIART);
+
+            using var httpClient = new HttpClient();
+            ICepService cepService = new CepService(httpClient);
+
+            string info = rainbow.Markup("CEP: ");
+            Console.Write(info);
+            string cep = Console.ReadLine();
+
+            try
+            {
+                CepInfo cepInfo = await cepService.GetCepInfoAsync(cep);
+
+                // Abra uma issue se souber uma forma de evitar esse monte de Console.WriteLine (:
+                Console.WriteLine("========================");
+                rainbow.WriteLineWithMarkup($"CEP: {cepInfo.cep}");
+                rainbow.WriteLineWithMarkup($"Estado: {cepInfo.estado}");
+                rainbow.WriteLineWithMarkup($"Regiao: {cepInfo.regiao}");
+                rainbow.WriteLineWithMarkup($"Localidade: {cepInfo.localidade}");
+                rainbow.WriteLineWithMarkup($"Logradouro: {cepInfo.logradouro}");
+                rainbow.WriteLineWithMarkup($"UF: {cepInfo.uf}");
+                rainbow.WriteLineWithMarkup($"DDD: {cepInfo.ddd}");
+                Console.WriteLine("========================");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# ViaCEPCLI
+Versão CLI do ViaCEP, não oficial.
+
+# Demo
+
+![demo](https://github.com/user-attachments/assets/6f980791-8ed3-4acd-8bd7-a983666ab61b)
